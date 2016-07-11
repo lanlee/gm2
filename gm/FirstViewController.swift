8e054de8d8c8e9e90097cde6c8e8705f7c623de9
Lan Li <lanli@lans-macbook-pro.local>
2016-07-11 15:06:59 +0800

    Initial Commit

---
+++ gm/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  gm
+//
+//  Created by Lan the Awesome Kid on 11/7/2016.
+//  Copyright © 2016年 Futurist. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gm/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  gm
+//
+//  Created by Lan the Awesome Kid on 11/7/2016.
+//  Copyright © 2016年 Futurist. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
